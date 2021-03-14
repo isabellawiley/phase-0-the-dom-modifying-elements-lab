5c626b73af2a84bda1f354869d44f03a90d3929d
Isabella <wileyisabella@gmail.com>
2021-03-14 17:10:16 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+const victory = document.createElement('id');
+newHeader.appendChild(victory);
+const node = document.createTextNode("YOUR-NAME is the champion");
+victory.appendChild(node);
+
+
+/* // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+const victory = document.createElement('id');
+newHeader.appendChild(victory);
+const node = document.createTextNode("YOUR-NAME is the champion");
+victory.appendChild(node);
+*/
